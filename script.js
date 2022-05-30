92398abf12803554af64c6efb5d6b8b2248e7bf9
shahir-abdullah <shahir2561@gmail.com>
2022-05-30 20:55:04 +0600

    stopwatch feature added

--- script.js
+++ script.js
@@ -1,9 +1,70 @@
 const btn = document.getElementById("btnIncrement");
 const textArea = document.getElementById("txtCnt");
+const timerBody = document.getElementById("timerId");
+const startBtn = document.getElementById("btnStart");
+const stopBtn = document.getElementById("btnStop");
+
+timerBody.innerHTML = '00:00:00';
 textArea.innerHTML = '0';
+
+let interval;
+let times = timerBody.innerHTML.split(":");
+let s = 0;
+let m = 0;
+let h = 0;
 let cnt = 0;
+let startClicked = false;
+
+
+function timer(){
+    times = timerBody.innerHTML.split(":");
+    s = parseInt(times[2]);
+    m = parseInt(times[1]);
+    h = parseInt(times[0]);
+    s++;
+    if(s>60){
+        s = 0;
+        m++;
+        if(m>60){
+            m = 0;
+            h++;
+        }
+    }
+    var hh, ss, mm;
+    if(s < 10){
+        ss = "0" + s;
+    }else{
+        ss = s;
+    }
+    if(m < 10){
+        mm = "0"+m;
+    }else{
+        mm = m;
+    }
+    if(h < 10){
+        hh = "0"+h;
+    }else{
+        hh = h;
+    }
+    timerBody.innerHTML = hh+":"+mm+":"+ss;
+};
+
+
+stopBtn.addEventListener("click", () => {
+    clearInterval(interval);
+    startClicked = false;
+});
+
+startBtn.addEventListener("click", () => {
+    if(startClicked==false){
+        interval = setInterval(timer, 1000);
+        startClicked = true;
+    }
+    
+});
+
 btn.addEventListener("click", () => {
-    //console.log("hello");
     cnt++;
     textArea.innerHTML =  cnt;
 });
+
